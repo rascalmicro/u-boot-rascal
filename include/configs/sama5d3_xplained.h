55a7241c79ea0360753a1eb11e0ca0af04c2bad0
Bo Shen <voice.shen@atmel.com>
2015-01-28 16:32:45 +0800

    ARM: atmel: switch to usb ehci for sama5d3 boards
    As the cache coherence issue in OHCI HCD, when enable I/D cache
    for sama5d3 SoC, the OHCI can not work properly. So, switch to
    EHCI, then the USB can work well.
    
    Signed-off-by: Bo Shen <voice.shen@atmel.com>

--- include/configs/sama5d3_xplained.h
+++ include/configs/sama5d3_xplained.h
@@ -40,17 +40,6 @@
 #define CONFIG_USART_BASE		ATMEL_BASE_DBGU
 #define CONFIG_USART_ID			ATMEL_ID_DBGU
 
-/*
- * This needs to be defined for the OHCI code to work but it is defined as
- * ATMEL_ID_UHPHS in the CPU specific header files.
- */
-#define ATMEL_ID_UHP			ATMEL_ID_UHPHS
-
-/*
- * Specify the clock enable bit in the PMC_SCER register.
- */
-#define ATMEL_PMC_UHP			AT91SAM926x_PMC_UHP
-
 #define CONFIG_BOOTDELAY		3
 
 /*
@@ -138,13 +127,9 @@
 #define CONFIG_CMD_USB
 
 #ifdef CONFIG_CMD_USB
-#define CONFIG_USB_ATMEL
-#define CONFIG_USB_ATMEL_CLK_SEL_UPLL
-#define CONFIG_USB_OHCI_NEW
-#define CONFIG_SYS_USB_OHCI_CPU_INIT
-#define CONFIG_SYS_USB_OHCI_REGS_BASE		ATMEL_BASE_OHCI
-#define CONFIG_SYS_USB_OHCI_SLOT_NAME		"SAMA5D3 Xplained"
-#define CONFIG_SYS_USB_OHCI_MAX_ROOT_PORTS	2
+#define CONFIG_USB_EHCI
+#define CONFIG_USB_EHCI_ATMEL
+#define CONFIG_SYS_USB_EHCI_MAX_ROOT_PORTS	3
 #define CONFIG_DOS_PARTITION
 #define CONFIG_USB_STORAGE
 #endif
