a88f3980c40f4880eceb42cac623d5a6c5f23e4b
Wenyou Yang <wenyou.yang@atmel.com>
2015-05-27 17:42:19 +0800

    drivers: mmc: atmel: add atmel sdhci support
    Signed-off-by: Wenyou Yang <wenyou.yang@atmel.com>

---
+++ arch/arm/include/asm/arch-at91/atmel_sdhci.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2015 Atmel Corporation
+ *
+ * SPDX-License-Identifier:     GPL-2.0+
+ */
+
+#ifndef	ATMEL_SDHCI_H
+#define	ATMEL_SDHCI_H
+
+int atmel_sdhci_init(void *regbase);
+
+#endif
---
+++ drivers/mmc/atmel_sdhci.c
@@ -0,0 +1,33 @@
+/*
+ * Copyright (C) 2015 Atmel Corporation
+ *			Wenyou.Yang <wenyou.yang@atmel.com>
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#include <common.h>
+#include <malloc.h>
+#include <sdhci.h>
+
+int atmel_sdhci_init(void *regbase)
+{
+	struct sdhci_host *host = NULL;
+
+	host = (struct sdhci_host *)malloc(sizeof(struct sdhci_host));
+	if (!host) {
+		printf("atmel_sdhci_init: sdhci_host malloc fail\n");
+		return 1;
+	}
+
+	host->name = "atmel_sdhci";
+	host->ioaddr = (void *)regbase;
+	host->quirks = SDHCI_QUIRK_BROKEN_R1B;
+	host->version = sdhci_readw(host, SDHCI_HOST_VERSION);
+	host->voltages = MMC_VDD_32_33 | MMC_VDD_33_34 | MMC_VDD_165_195;
+
+	host->host_caps = MMC_MODE_HC;
+
+	add_sdhci(host, 0, 0);
+
+	return 0;
+}
